ee6998c10b26137fb7eb137b4f417ddb65dba8c5
cri98li <cri98li@gmail.com>
2022-12-12 17:01:56 +0100

    Added more features and different "width" to the sliding window

--- Cage8/AggregateFeatures.py
+++ Cage8/AggregateFeatures.py
@@ -1,49 +1,131 @@
 import numpy as np
+import scipy
+def interpolate(val1, time1, val2, time2, time):
+    interp = scipy.interpolate.interp1d([time1, time2], [val1, val2])
+
+    return interp(time)
 
 
-def _rolling_window(a, window):
-    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
-    strides = a.strides + (a.strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+#window_type= None-> #features; 'time'-> time based; 'distance'-> distance based
+def _rolling_window(a, window, window_type, time=None, distance=None):
+    if window_type is None:
+        shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+        strides = a.strides + (a.strides[-1],)
+        for row in np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides):
+            yield row
+    elif window_type == 'time': #TODO: optimize
+        if time is None:
+            raise ValueError( f"time cannot be None when window_type={window_type}")
+        for i in range(len(time-1)):
+            total_time = 0
+            values = []
+
+            for j, corr, succ in enumerate(zip(time[i:-1], time[i+1:])):
+                delta = succ - corr
+                if total_time + delta > window:
+                    interpolated_value = interpolate(features[i+j], corr, features[i+j+1], succ, window)
+                    values.append(interpolated_value)
+                    break
+                else:
+                    total_time += delta
+                    values.append(features[i+j])
+            yield values
+    elif window_type == 'distance': #TODO: optimize
+        if distance is None:
+            raise ValueError(f"distance cannot be None when window_type={window_type}")
+        for i in range(len(distance - 1)):
+            total_distance = 0
+            values = []
+
+            for j, corr, succ in enumerate(zip(distance[i:-1], distance[i + 1:])):
+                delta = succ - corr
+                if total_distance + delta > window:
+                    interpolated_value = interpolate(features[i + j], corr, features[i + j + 1], succ, window)
+                    values.append(interpolated_value)
+                    break
+                else:
+                    total_distance += delta
+                    values.append(features[i + j])
+            yield values
+    else:
+        raise ValueError(f"window_type={window_type} unsupported. window_type must be in [None, 'time', 'distance']")
 
 
-def sum(features: np.ndarray, window):
+def sum(features: np.ndarray, window, window_type = None, time=None, distance=None):
     if window is None:
         window = len(features)
 
-    np.sum(_rolling_window(features, window))
+    #np.sum(_rolling_window(features, window, window_type))
+
+    return np.sum(np.fromiter(_rolling_window(features, window, window_type, time, distance), dtype=np.dtype((float, 3))), axis=1)
 
 
-def max(features: np.ndarray, window):
+def max(features: np.ndarray, window, window_type = None, time=None, distance=None):
     if window is None:
         window = len(features)
 
-    np.max(_rolling_window(features, window))
+    return np.max(np.fromiter(_rolling_window(features, window, window_type, time, distance), dtype=np.dtype((float, 3))), axis=1)
 
 
-def min(features: np.ndarray, window):
+def min(features: np.ndarray, window, window_type = None, time=None, distance=None):
     if window is None:
         window = len(features)
 
-    np.min(_rolling_window(features, window))
+    return np.min(np.fromiter(_rolling_window(features, window, window_type, time, distance), dtype=np.dtype((float, 3))), axis=1)
 
 
-def std(features: np.ndarray, window):
+def std(features: np.ndarray, window, window_type = None, time=None, distance=None):
     if window is None:
         window = len(features)
 
-    np.std(_rolling_window(features, window))
+    return np.std(np.fromiter(_rolling_window(features, window, window_type, time, distance), dtype=np.dtype((float, 3))), axis=1)
 
-
-def cov(features: np.ndarray, window):
+def cov(features: np.ndarray, window, window_type = None, time=None, distance=None):
     if window is None:
         window = len(features)
 
-    np.cov(_rolling_window(features, window))
+    return np.cov(np.fromiter(_rolling_window(features, window, window_type, time, distance), dtype=np.dtype((float, 3))), axis=1)
 
-
-def var(features: np.ndarray, window):
+def var(features: np.ndarray, window, window_type = None, time=None, distance=None):
     if window is None:
         window = len(features)
 
-    np.var(_rolling_window(features, window))
+    return np.var(np.fromiter(_rolling_window(features, window, window_type, time, distance), dtype=np.dtype((float, 3))), axis=1)
+
+def rate_upper(features: np.ndarray, threshold, window, distance=None, time=None, window_type = None):
+    if window is None:
+        window = len(features)
+
+    returnValue = np.zeros(len(features) - window + 1)
+
+    """count = 0
+    for i in range(len(features)):
+
+        if features[i] > threshold:
+            count += 1
+        if i - window + 1 > 0 and features[i - window] > threshold:
+            count -= 1
+        if i - window + 1 >= 0:
+            returnValue[i - window + 1] = count"""
+
+    for i, window_value in enumerate(_rolling_window(features, window, window_type, time, distance)):
+        for el in window_value:
+            if el > threshold:
+                returnValue[i] += 1
+
+    if distance is not None:
+        returnValue /= distance
+
+    return returnValue
+
+
+def rate_below(features: np.ndarray, threshold, window, distance=None, time=None, window_type = None):
+    return rate_upper(features=features, threshold=threshold * -1, window=window, window_type=window_type,
+                      distance=distance, time=time)
+
+
+features = np.array([1,1,1,2,3,2,2,3,6,7])
+
+res = sum(features, 3)
+
+print(f"result({type(res)}): {res}")
--- Cage8/BasicFeatures.py
+++ Cage8/BasicFeatures.py
@@ -1,4 +1,5 @@
 import numpy as np
+from geopy import distance
 
 
 def speed(lat: np.ndarray, lon: np.ndarray, time: np.ndarray):
@@ -34,6 +35,14 @@ def direction(lat: np.ndarray, lon: np.ndarray):
     dist_lon = lon[1:] - lon[:-1]
 
     return np.append([.0], np.arctan2(dist_lat, dist_lon))
+
+
+def distance(lat: np.ndarray, lon: np.ndarray, ellipsoid="WGS-84"):
+    returnValue = np.zeros(len(lat) - 1)
+    for i in range(len(lat) - 1):
+        returnValue[i] = distance.geodesic((lat[i], lon[i]), (lat[i + 1], lon[i + 1]), ellipsoid=ellipsoid).meters
+
+    return np.append([.0], returnValue)
 
 
 lat = np.array([1, 2, 2])
